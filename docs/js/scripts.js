d3330470c7016ee5d02c90d5eb0ba455197df65b
Justin Golden <justingolden21@gmail.com>
2021-06-29 19:31:44 -1000

    finished styling website

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,1 +1,9 @@
-window.onload = ()=> console.log('page loaded');
+window.onload = () => {
+	const weekday = new Date().getDay();
+	if (weekday == 0) document.getElementById('sun').classList.add('highlight');
+	else if (weekday == 5 || weekday == 6)
+		document.getElementById('fri-sat').classList.add('highlight');
+	else document.getElementById('mon-thurs').classList.add('highlight');
+
+	document.getElementById(window.location.hash.substring(1)).scrollIntoView();
+};
