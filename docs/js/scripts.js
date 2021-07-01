c88ea28680825e6685a0e60950d8f32e0433a5d0
Justin Golden <justingolden21@gmail.com>
2021-06-30 14:07:38 -1000

    add hamburger menu

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,4 +1,5 @@
 window.onload = () => {
+	// highlight hours
 	const weekday = new Date().getDay();
 	let weekname;
 	if (weekday == 0) weekname = 'sun';
@@ -6,6 +7,14 @@ window.onload = () => {
 	else weekname = 'mon-thurs';
 	document.getElementById(weekname).classList.add('highlight');
 
+	// scroll to hash
 	const elm = window.location.hash.substring(1);
 	if (elm && elm.length) document.getElementById(elm).scrollIntoView();
+
+	// mobile nav
+	document.getElementById('menu-btn').addEventListener('click', () => {
+		document.getElementById('menu').classList.toggle('hidden');
+		document.getElementById('close').classList.toggle('hidden');
+		document.querySelector('nav').classList.toggle('active');
+	});
 };
